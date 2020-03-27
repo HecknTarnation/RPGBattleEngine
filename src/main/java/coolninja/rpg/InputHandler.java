389f13af37061cc03e08ae8c53445edc0c0353fc
Ben Ballard <coolninjagames@gmail.com>
2020-03-26 22:58:21 -0400

    Updated

--- src/main/java/coolninja/rpg/InputHandler.java
+++ src/main/java/coolninja/rpg/InputHandler.java
@@ -5,32 +5,48 @@ import java.util.Scanner;
 
 /**
  * The built-in input handler
+ *
  * @version 1.0
  * @since 1.0
  * @author Ben Ballard
  */
 public class InputHandler {
-    
+
     private static final Scanner scan = new Scanner(System.in);
-    
+
     /**
      * Gets the input of the player as a string
+     *
      * @since 1.0
      * @return string
      */
-    public static String getInput(){
+    public static String getInput() {
+        System.out.print("Input: ");
         return scan.nextLine();
     }
-    
+
     /**
      * Waits for the user to press enter
+     *
      * @since 1.0
      */
-    public static void pressEnter(){
+    public static void pressEnter() {
         System.out.println("Press Enter key to continue...");
         try {
             System.in.read();
-        }catch (IOException e){}
+        } catch (IOException e) {
+        }
     }
-    
+
+    /**
+     * Used for Yes or No questions. Returns true for yes.
+     *
+     * @return boolean
+     * @since 1.0
+     */
+    public static boolean getYON() {
+        System.out.print("(Y) or (N):");
+        return scan.nextLine().equalsIgnoreCase("Y");
+    }
+
 }
