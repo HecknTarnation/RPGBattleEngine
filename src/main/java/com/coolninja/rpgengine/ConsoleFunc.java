e43133fd2462173ec61fe22252dc1962c2c4af80
Ben Ballard <coolninjagames@gmail.com>
2021-03-20 00:11:37 -0400

    Added "dots" method

--- src/main/java/com/coolninja/rpgengine/ConsoleFunc.java
+++ src/main/java/com/coolninja/rpgengine/ConsoleFunc.java
@@ -21,4 +21,17 @@ public class ConsoleFunc {
         System.out.print("\033[H\033[2J");
     }
 
+    public static void dots(String str, int dotCount, long timeBetween) {
+        System.out.print(str);
+        for (int i = 0; i < dotCount; i++) {
+            System.out.print(".");
+            try {
+                Thread.sleep(timeBetween);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ConsoleFunc.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        System.out.print("\n");
+    }
+
 }
