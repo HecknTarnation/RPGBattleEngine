8f31d2ab600d8839ae9f1eae7a51b1273004a9fd
Ben Ballard <coolninjagames@gmail.com>
2021-03-21 20:38:07 -0400

    Update EN_USKeys.java

--- src/main/java/com/coolninja/rpgengine/enums/EN_USKeys.java
+++ src/main/java/com/coolninja/rpgengine/enums/EN_USKeys.java
@@ -10,7 +10,11 @@ public enum EN_USKeys {
     battle_compidle("battle_compidle"),
     battle_plridle("battle_plridle"),
     battle_menu("battle_menu"),
-    battle_currentTurn("battle_currentTurn");
+    battle_currentTurn("battle_currentTurn"),
+    /**
+     * "[p1] used [p2] and hit [p3] for [p4] damage!"
+     */
+    battle_moveused("battle_moveused");
 
     public String key;
 
