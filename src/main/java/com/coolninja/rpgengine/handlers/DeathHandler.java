5b58d02e69110e21f4a5dccf29bf2a38bf27da30
Ben Ballard <coolninjagames@gmail.com>
2021-08-10 15:55:18 -0400

    Update DeathHandler.java

--- src/main/java/com/coolninja/rpgengine/handlers/DeathHandler.java
+++ src/main/java/com/coolninja/rpgengine/handlers/DeathHandler.java
@@ -1,14 +1,15 @@
 package com.coolninja.rpgengine.handlers;
 
 /**
- *
+ * @version 1.0
+ * @since 1.0
  * @author Ben
  */
 public class DeathHandler {
-    
+
     public void OnDeath() {
         System.out.println("You should override this handler!");
         System.exit(0);
     }
-    
+
 }
