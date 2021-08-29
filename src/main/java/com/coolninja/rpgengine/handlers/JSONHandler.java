3792a6ec1336799430dfce390c811038c7bab7e8
Ben Ballard <coolninjagames@gmail.com>
2021-08-29 16:53:07 -0400

    Create JSONHandler.java

---
+++ src/main/java/com/coolninja/rpgengine/handlers/JSONHandler.java
@@ -0,0 +1,20 @@
+package com.coolninja.rpgengine.handlers;
+
+import org.json.simple.*;
+
+/**
+ *
+ * @author Ben
+ */
+//TODO: write
+public class JSONHandler {
+
+    public JSONObject toJson(Object obj) {
+        return null;
+    }
+
+    public <T> T fromJson(JSONObject file) {
+        return null;
+    }
+
+}
