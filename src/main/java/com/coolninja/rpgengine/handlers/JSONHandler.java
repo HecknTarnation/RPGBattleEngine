3db16cf891e6c7343d7c11a029d72a9fbc40dad6
Ben Ballard <coolninjagames@gmail.com>
2021-09-14 02:24:51 -0400

    Update JSONHandler.java
    I have no idea what I'm doing.

--- src/main/java/com/coolninja/rpgengine/handlers/JSONHandler.java
+++ src/main/java/com/coolninja/rpgengine/handlers/JSONHandler.java
@@ -1,5 +1,8 @@
 package com.coolninja.rpgengine.handlers;
 
+import com.coolninja.rpgengine.Cons.*;
+import com.coolninja.rpgengine.arrays.StatusArray;
+import com.coolninja.rpgengine.enums.StatusArrayPosition;
 import org.json.simple.*;
 
 /**
@@ -13,7 +16,17 @@ public class JSONHandler {
         return null;
     }
 
-    public <T> T fromJson(JSONObject file) {
+    public Object fromJson(JSONObject file) {
+        String type = (String) file.get("type");
+        type = type.toLowerCase();
+        String name = (String) file.get("name");
+        switch (type) {
+            case "enemy":
+                int expVal = file.get("expVal") != null ? (int) file.get("expVal") : 0;
+                StatusArray arr = StatusArray.fromJSONArr((JSONObject) file.get("stats"));
+                Drop[] drops = null;
+                return new Enemy(name, (int) arr.get(StatusArrayPosition.Health), expVal).setStats(arr).setDrop(drops);
+        }
         return null;
     }
 
