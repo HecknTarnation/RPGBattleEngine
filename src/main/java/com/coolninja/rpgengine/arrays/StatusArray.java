f5f12bceafbd5eae9b1b2d15a497c4643935e5a4
Ben Ballard <coolninjagames@gmail.com>
2021-09-14 02:26:03 -0400

    Update StatusArray.java

--- src/main/java/com/coolninja/rpgengine/arrays/StatusArray.java
+++ src/main/java/com/coolninja/rpgengine/arrays/StatusArray.java
@@ -1,5 +1,7 @@
 package com.coolninja.rpgengine.arrays;
 
+import com.coolninja.rpgengine.Cons.StatusEffect;
+import com.coolninja.rpgengine.Cons.Weakness;
 import com.coolninja.rpgengine.enums.StatusArrayPosition;
 import java.util.ArrayList;
 import org.json.simple.JSONObject;
@@ -40,9 +42,12 @@ public class StatusArray {
         arr.modify(StatusArrayPosition.Luck, json.get("luck"));
         arr.modify(StatusArrayPosition.MATK, json.get("magicAttack"));
         arr.modify(StatusArrayPosition.MDEF, json.get("magicDefense"));
-        arr.modify(StatusArrayPosition.Weakness, json.get("weakness"));
+        //TODO: Figure out how to do
+        arr.modify(StatusArrayPosition.Weakness, (Weakness[]) json.get("weakness"));
         arr.modify(StatusArrayPosition.AILevel, json.get("aiLevel"));
-        arr.modify(StatusArrayPosition.StatusEffect, json.get("statusEffect"));
+        //TODO: Figure out how to do
+        arr.modify(StatusArrayPosition.StatusEffect, (StatusEffect[]) json.get("statusEffect"));
+        arr.modify(StatusArrayPosition.Evasion, json.get("evasion"));
 
         return arr;
     }
