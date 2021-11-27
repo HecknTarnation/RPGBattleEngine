e79662aafb0cb264a48bc5ea507cabd992163ffb
Ben Ballard <coolninjagames@gmail.com>
2021-11-27 01:58:21 -0500

    Create battle_handler.java

---
+++ src/main/java/com/heckntarnation/rpgbattleengine/lua/battle_handler.java
@@ -0,0 +1,38 @@
+package com.heckntarnation.rpgbattleengine.lua;
+
+import com.heckntarnation.rpgbattleengine.BattleEngine;
+import com.heckntarnation.rpgbattleengine.exceptions.IncorrectObjectRecieved;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.TwoArgFunction;
+
+/**
+ *
+ * @author Ben
+ */
+public class battle_handler extends TwoArgFunction {
+
+    @Override
+    public LuaValue call(LuaValue modname, LuaValue env) {
+        LuaValue library = tableOf();
+        library.set("", new start_battle());
+        env.set("battle_handler", library);
+        return library;
+    }
+
+    static class start_battle extends TwoArgFunction {
+
+        @Override
+        public LuaValue call(LuaValue canRun, LuaValue enemies) {
+            try {
+                BattleEngine.battleHandler.startBattle(canRun.checkboolean(), enemies.checktable());
+            } catch (IncorrectObjectRecieved ex) {
+                Logger.getLogger(battle_handler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return null;
+        }
+
+    }
+
+}
