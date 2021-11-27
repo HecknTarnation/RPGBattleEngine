614263ee9ab83f0e7c7bc8f64ab3f19352fec4d2
Ben Ballard <coolninjagames@gmail.com>
2021-11-27 01:58:48 -0500

    Create IncorrectObjectRecieved.java

---
+++ src/main/java/com/heckntarnation/rpgbattleengine/exceptions/IncorrectObjectRecieved.java
@@ -0,0 +1,13 @@
+package com.heckntarnation.rpgbattleengine.exceptions;
+
+/**
+ *
+ * @author Ben
+ */
+public class IncorrectObjectRecieved extends Exception {
+
+    public IncorrectObjectRecieved(String msg) {
+        super(msg);
+    }
+
+}
