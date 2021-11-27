bc93079b9c6c4f9a79d4d78c9c700fa33e41550a
Ben Ballard <coolninjagames@gmail.com>
2021-11-27 01:58:26 -0500

    Create LuaHandler.java

---
+++ src/main/java/com/heckntarnation/rpgbattleengine/handlers/LuaHandler.java
@@ -0,0 +1,25 @@
+package com.heckntarnation.rpgbattleengine.handlers;
+
+import org.luaj.vm2.Globals;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.jse.JsePlatform;
+
+/**
+ * This class handlers Lua scripting.
+ *
+ * @author Ben
+ */
+public class LuaHandler {
+
+    public Globals globals;
+
+    public void init() {
+        globals = JsePlatform.standardGlobals();
+    }
+
+    public void runscript(String scriptPath) {
+        LuaValue chunk = globals.loadfile(scriptPath);
+        chunk.call();
+    }
+
+}
