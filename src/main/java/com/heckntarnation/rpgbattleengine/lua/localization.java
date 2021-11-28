a2a82a92e62501a2d353ee388213e9c1b8a78e84
Ben Ballard <coolninjagames@gmail.com>
2021-11-27 21:51:34 -0500

    Update localization.java

--- src/main/java/com/heckntarnation/rpgbattleengine/lua/localization.java
+++ src/main/java/com/heckntarnation/rpgbattleengine/lua/localization.java
@@ -1,6 +1,7 @@
 package com.heckntarnation.rpgbattleengine.lua;
 
 import com.heckntarnation.rpgbattleengine.BattleEngine;
+import com.heckntarnation.rpgbattleengine.exceptions.StringAlreadyDefinedException;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.*;
 
@@ -16,7 +17,7 @@ public class localization extends TwoArgFunction {
     @Override
     public LuaValue call(LuaValue modname, LuaValue env) {
         LuaValue library = tableOf();
-        library.set("getString", new get_string());
+        library.set("get_string", new get_string());
         env.set("localization", library);
         return library;
     }
@@ -26,6 +27,20 @@ public class localization extends TwoArgFunction {
         @Override
         public LuaValue call(LuaValue lv) {
             return LuaValue.valueOf(BattleEngine.localizationHandler.getLocalizedString(lv.checkstring().toString()));
+        }
+
+    }
+
+    static class add_string extends TwoArgFunction {
+
+        @Override
+        public LuaValue call(LuaValue key, LuaValue value) {
+            try {
+                BattleEngine.localizationHandler.addString(key.checkstring().toString(), value.checkstring().toString());
+            } catch (StringAlreadyDefinedException ex) {
+                return LuaValue.valueOf(false);
+            }
+            return LuaValue.valueOf(true);
         }
 
     }
