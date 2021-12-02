ec32670a33271cb42a727c3bc231c4bcf3670668
Ben Ballard <coolninjagames@gmail.com>
2021-12-01 21:58:29 -0500

    Update JSONHandler.java

--- src/main/java/com/heckntarnation/rpgbattleengine/handlers/JSONHandler.java
+++ src/main/java/com/heckntarnation/rpgbattleengine/handlers/JSONHandler.java
@@ -1,8 +1,8 @@
 package com.heckntarnation.rpgbattleengine.handlers;
 
 import com.heckntarnation.rpgbattleengine.BattleEngine;
-import com.heckntarnation.rpgbattleengine.Cons.*;
 import com.heckntarnation.rpgbattleengine.arrays.StatusArray;
+import com.heckntarnation.rpgbattleengine.cons.*;
 import com.heckntarnation.rpgbattleengine.enums.EquipSlot;
 import com.heckntarnation.rpgbattleengine.exceptions.ObjectAlreadyLoadedException;
 import java.io.File;
@@ -29,8 +29,13 @@ public class JSONHandler {
     public final String DROP = "drop";
     public final String GRAPHIC = "graphic";
     public final String EQUIPMENT = "equipment";
+    public final String COMPANION = "companion";
+    public final String PLAYER = "player";
+    public final String MOVE = "move";
+    public final String STATUS_EFFECT = "status_effect";
+    public final String WEAKNESS = "weakness";
 
-    public Object getObject(String key) {
+    public TypedObject getObject(String key) {
         return loadedObjects.get(key);
     }
 
@@ -52,19 +57,19 @@ public class JSONHandler {
 
     private final ArrayList<JSONObject> loadLater = new ArrayList<>();
 
-    public Object[] fromJSON(File[] files) throws ObjectAlreadyLoadedException {
-        ArrayList<Object> objs = new ArrayList<>();
+    public TypedObject[] fromJSON(File[] files) throws ObjectAlreadyLoadedException {
+        JSONObject[] jsonObjs = new JSONObject[files.length];
         JSONParser parser = new JSONParser();
-        for (File f : files) {
+        for (int i = 0; i < files.length; i++) {
             try {
-                objs.add(fromJSON((JSONObject) parser.parse(new FileReader(f))));
+                jsonObjs[i] = (JSONObject) parser.parse(new FileReader(files[i]));
             } catch (FileNotFoundException ex) {
                 Logger.getLogger(JSONHandler.class.getName()).log(Level.SEVERE, null, ex);
             } catch (IOException | ParseException ex) {
                 Logger.getLogger(JSONHandler.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        return objs.toArray();
+        return fromJSON(jsonObjs);
     }
 
     public TypedObject[] fromJSON(JSONObject[] files) throws ObjectAlreadyLoadedException {
@@ -120,6 +125,9 @@ public class JSONHandler {
         String[] namespacedid = ((String) file.get("id")).split(":");
         namespace = namespacedid[0];
         id = namespacedid[1];
+        if (getObject(namespace + ":" + id) != null) {
+            throw new ObjectAlreadyLoadedException(namespace + ":" + id);
+        }
         String type = (String) file.get("type");
         switch (type) {
             case ITEM: {
@@ -138,7 +146,7 @@ public class JSONHandler {
                 break;
             }
             case ENEMY: {
-                Enemy en = new Enemy("", (int) file.get("expVal"));
+                Enemy en = new Enemy((String) file.get("name"), (int) file.get("expVal"));
                 en.namespace = namespace;
                 en.id = id;
                 en.setStats(StatusArray.fromJSONObj(file));
@@ -165,6 +173,8 @@ public class JSONHandler {
                     return LOAD_LATER;
                 }
                 Drop drop = new Drop(it, (String) file.get("chance"));
+                drop.namespace = namespace;
+                drop.id = id;
                 obj = drop;
                 break;
             }
@@ -175,24 +185,79 @@ public class JSONHandler {
                 String[] frameArray = new String[jsonArr.size()];
                 frameArray = (String[]) jsonArr.toArray(frameArray);
                 gr.setFrames(frameArray);
+                gr.namespace = namespace;
+                gr.id = id;
                 obj = gr;
                 break;
             }
             case EQUIPMENT: {
                 EquipSlot slot = EquipSlot.getFromString((String) file.get("slot"));
                 Equipment equip = new Equipment((String) file.get("name"), slot);
+                equip.namespace = namespace;
+                equip.id = id;
                 obj = equip;
                 break;
             }
-        }
-        if (getObject(namespace + ":" + id) != null) {
-            throw new ObjectAlreadyLoadedException(namespace + ":" + id);
+            case COMPANION: {
+                Companion comp = new Companion((String) file.get("name"), (int) file.get("baseExp"), (int) file.get("expMod"));
+                comp = (Companion) loadPlayer(comp, file);
+                if (comp == LOAD_LATER.object) {
+                    return LOAD_LATER;
+                }
+                comp.namespace = namespace;
+                comp.id = id;
+                obj = comp;
+                break;
+            }
+            case PLAYER: {
+                Player plr = new Player((String) file.get("name"), (int) file.get("baseExp"), (int) file.get("expMod"));
+                plr = loadPlayer(plr, file);
+                if (plr == LOAD_LATER.object) {
+                    return LOAD_LATER;
+                }
+                plr.namespace = namespace;
+                plr.id = id;
+                obj = plr;
+                break;
+            }
         }
         TypedObject tObj = new TypedObject(type, obj);
         tObj.namespace = namespace;
         tObj.id = id;
         loadedObjects.put(namespace + ":" + id, tObj);
         return tObj;
+    }
+
+    private Player loadPlayer(Player obj, JSONObject file) {
+        obj.level = (int) file.get("level");
+        obj.growthRates = (double[]) file.get("growthRates");
+        obj.health = (int) file.get("health");
+        obj.maxHealth = (int) file.get("maxHealth");
+        obj.mana = (int) file.get("mana");
+        obj.attack = (int) file.get("attack");
+        obj.mAttack = (int) file.get("mAttack");
+        obj.luck = (int) file.get("luck");
+        obj.defense = (int) file.get("defense");
+        obj.mDefense = (int) file.get("mDefense");
+        obj.exp = (int) file.get("exp");
+        obj.expToNextLevel = (int) file.get("expToNextLevel");
+        obj.evasion = (float) file.get("evasion");
+        JSONArray moveIds = (JSONArray) file.get("moves");
+        ArrayList<Move> moves = new ArrayList<>();
+        for (Object o : moveIds) {
+            String id = (String) o;
+            Move m = (Move) getObject(id).object;
+            if (m == null) {
+                return (Player) LOAD_LATER.object;
+            }
+            moves.add(m);
+        }
+        obj.moves = moves;
+        //TODO: equipment
+        //TODO: status effect
+        //TODO: inv
+        //TODO: weaknesses
+        return obj;
     }
 
     public class TypedObject {
