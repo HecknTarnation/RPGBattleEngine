9ebd2b394eb00e7b06fc1211ecdb42d7da0591ea
Ben Ballard <coolninjagames@gmail.com>
2021-12-06 22:57:22 -0500

    Update Companion.java

--- src/main/java/com/heckntarnation/rpgbattleengine/Cons/Companion.java
+++ src/main/java/com/heckntarnation/rpgbattleengine/Cons/Companion.java
@@ -1,12 +1,19 @@
 package com.heckntarnation.rpgbattleengine.cons;
 
+import com.heckntarnation.rpgbattleengine.Vars;
+
 /**
  *
  * @author Ben
  */
 public class Companion extends Player {
-
+    
     public Companion(String name, int baseExp, int expMod) {
         super(name, baseExp, expMod);
+    }
+    
+    @Override
+    public void addItemToInv(Item item) {
+        Vars.player.addItemToInv(item);
     }
 }
