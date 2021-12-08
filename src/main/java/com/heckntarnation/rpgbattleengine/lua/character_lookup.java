1fa88de032e2c2c4802a8d3d239ae58c946b5b3c
Ben Ballard <coolninjagames@gmail.com>
2021-12-07 21:16:45 -0500

    Create character_lookup.java

---
+++ src/main/java/com/heckntarnation/rpgbattleengine/lua/character_lookup.java
@@ -0,0 +1,75 @@
+package com.heckntarnation.rpgbattleengine.lua;
+
+import com.heckntarnation.rpgbattleengine.BattleEngine;
+import com.heckntarnation.rpgbattleengine.Vars;
+import com.heckntarnation.rpgbattleengine.cons.Companion;
+import com.heckntarnation.rpgbattleengine.cons.Enemy;
+import com.heckntarnation.rpgbattleengine.cons.Player;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.OneArgFunction;
+import org.luaj.vm2.lib.TwoArgFunction;
+
+/**
+ *
+ * @author Ben
+ */
+public class character_lookup extends TwoArgFunction {
+
+    public static Object lookup(int characterID) {
+        if (characterID == 0) {
+            return Vars.player;
+        } else if ((characterID - 1) < Vars.companions.length) {
+            return Vars.companions[characterID - 1];
+        } else {
+            return BattleEngine.battleHandler.ens[characterID - 1];
+        }
+    }
+
+    public static int lookupID(Object character) {
+        if (character instanceof Player) {
+            return 0;
+        } else if (character instanceof Companion) {
+            for (int i = 0; i < Vars.companions.length; i++) {
+                if ((Companion) character == Vars.companions[i]) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < BattleEngine.battleHandler.ens.length; i++) {
+                if ((Enemy) character == BattleEngine.battleHandler.ens[i]) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public LuaValue call(LuaValue modname, LuaValue env) {
+        LuaValue library = tableOf();
+        library.set("lookup", new lookup());
+        library.set("lookupID", new lookupID());
+        env.set("character_lookup", library);
+        return library;
+    }
+
+    static class lookup extends OneArgFunction {
+
+        @Override
+        public LuaValue call(LuaValue lv) {
+            return (LuaValue) lookup(lv.checkint());
+        }
+
+    }
+
+    static class lookupID extends OneArgFunction {
+
+        @Override
+        public LuaValue call(LuaValue lv) {
+            //TODO: this
+            return LuaValue.valueOf(lookupID(lv.checkint()));
+        }
+
+    }
+
+}
