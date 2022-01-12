5c55869271ce03f4f59b36d72835d67bae3d9ab9
Ben Ballard <coolninjagames@gmail.com>
2022-01-11 19:34:53 -0500

    Create CycleValueLong.java

---
+++ src/main/java/com/heckntarnation/rpgbattleengine/cons/CycleValues/CycleValueLong.java
@@ -0,0 +1,51 @@
+package com.heckntarnation.rpgbattleengine.cons.CycleValues;
+
+/**
+ *
+ * @author Ben
+ */
+public class CycleValueLong {
+
+    public long value;
+    public long min;
+    public long max;
+
+    public CycleValueLong(long initalValue, long minValue, long maxValue) {
+        this.value = initalValue;
+        this.min = minValue;
+        this.max = maxValue;
+    }
+
+    public CycleValueLong(long minValue, long maxValue) {
+        this.value = 0;
+        this.min = minValue;
+        this.max = maxValue;
+    }
+
+    public long getValue() {
+        adjustValue();
+        return this.value;
+    }
+
+    public long incValue() {
+        this.value++;
+        adjustValue();
+        return this.value;
+    }
+
+    public long decValue() {
+        this.value--;
+        adjustValue();
+        return this.value;
+    }
+
+    private void adjustValue() {
+        if (this.value > max) {
+            this.value = min;
+        }
+        if (this.value < min) {
+            this.value = max;
+        }
+    }
+
+}
