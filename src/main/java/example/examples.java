fabf1ab770b9b5b34213e321db61893883101864
Ben Ballard <coolninjagames@gmail.com>
2022-09-06 16:29:23 -0400

    Update examples.java

--- src/main/java/example/examples.java
+++ src/main/java/example/examples.java
@@ -1,6 +1,8 @@
 package example;
 
 import com.heckntarnation.rpgbattleengine.BattleEngine;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 /**
  *
@@ -8,12 +10,14 @@ import com.heckntarnation.rpgbattleengine.BattleEngine;
  */
 public class examples {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException, IOException {
         BattleEngine.init();
         System.out.println("Select example to run.");
         String[] menu = new String[]{
             "Basic Battle",
-            "Configs"
+            "Configs",
+            "Localization",
+            "Sounds"
         };
         int index = BattleEngine.inputHandler.doMenu(menu, "Select example to run.", true);
         switch (index) {
@@ -25,6 +29,9 @@ public class examples {
             }
             case 2: {
                 example.localization.LocalizationMain.main(args);
+            }
+            case 3: {
+                example.sounds.SoundsMain.main(args);
             }
         }
 
